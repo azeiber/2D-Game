c4b183ad7ae5a838c5b5cd2da14c7fd878170f38
Alexa <123220954+azeiber@users.noreply.github.com>
2024-10-17 22:43:11 -0400

    adding lab3

---
+++ Lab 3/node_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var speed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100, 200)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_right"):
+		position.x += speed;
+	if Input.is_action_just_pressed("ui_left"):
+		position.x -= speed;
