da0f5155561baebe740eff35d2cf20881c0c38f1
Ulixses10 <ulisesjm123@gmail.com>
2024-10-29 14:36:15 -0400

    Character Jumping(W, spacebar, Up_arrow) + Moving

--- 2d-game-project/animated_scene.gd
+++ 2d-game-project/animated_scene.gd
@@ -3,8 +3,15 @@ extends Node2D
 @onready var anim = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	anim.play("Run")
+	#anim.play("Run")
+	#anim.play("Jump")
+	pass	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Jump"):
+		anim.play("Jump")
+	elif !anim.is_playing(): #or anim.animation == "Jump":
+		anim.play("Run")
+	
+	
