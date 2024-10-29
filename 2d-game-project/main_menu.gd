97ed0f43d5eebb75f3af0ef29e54425876292086
Alexa <alexazeiber@gmail.com>
2024-10-29 13:39:02 -0400

    added sound and updated character script

--- 2d-game-project/main_menu.gd
+++ 2d-game-project/main_menu.gd
@@ -1,4 +1,4 @@
 extends Control
-const main_level = preload("res://main.tscn")
+const main_level = preload("res://level1.tscn")
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_packed(main_level)
