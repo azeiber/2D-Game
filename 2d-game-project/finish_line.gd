89a80c8871560de22d26f94d848db46a9e9ba90c
Ulixses10 <ulisesjm123@gmail.com>
2024-11-01 12:59:26 -0400

    Victory Scene Fixed

--- 2d-game-project/finish_line.gd
+++ 2d-game-project/finish_line.gd
@@ -5,3 +5,9 @@ const victory_scene = preload("res://victoryscene.tscn")
 func _on_Area2D_body_entered(body):
 	print("Body entered:", body.name)
 	get_tree().change_scene_to_packed(victory_scene)  # Change to the next scene
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Body entered:", body.name)
+	get_tree().change_scene_to_packed(victory_scene) 
+	 # Replace with function body.
